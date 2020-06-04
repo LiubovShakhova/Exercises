ea24936e5f08331556cab9a8ec7305562e5d7304
Liubov Shakhova <shakhova_l90@mail.ru>
2020-06-04 18:19:01 +0200

    Lesson01

---
+++ script/script.js
@@ -0,0 +1,16 @@
+const money = 120,
+    income = 300 + money,
+    addExpenses = true,
+    deposit = false,
+    mission = 'Get money',
+    period = 'Summer'; 
+
+/* Variant
+let money = 120;
+let income = money;
+let mission = 'Our goal is great';
+let period = ['Summer', 'Autumn', 'tomorrow'];
+*/
+
+alert('We begin to learn JS');
+console.log (`We will learn js in ${period}`);
