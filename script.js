ea9018cffa405758c66df13cf918c004ac376450
Liubov Shakhova <shakhova_l90@mail.ru>
2020-06-06 13:35:10 +0200

    2 Variable of Solution

--- script.js
+++ script.js
@@ -3,15 +3,38 @@
 let num = 266219;
 let arr = num.toString().split('');
 
-arr = arr.reduce((a,b) => a*b);
+// Result with Reduce
+/* arr = arr.reduce((a,b) => a*b);
 console.log(arr);
 
 let multiply = arr**3;
 console.log(multiply);
+
 multiply = String(multiply);
 console.log(multiply);
+
 console.log(multiply.substr(0, 2));
 alert(`${multiply.substr(0, 2)}`);
+ */
+
+//Variable with forEach
+let multiply = 1;
+arr.forEach(function(i, arr) {
+  multiply *= i;
+  console.log(multiply);
+});
+
+multiply = multiply**3;
+console.log(multiply);
+
+multiply = String(multiply);
+console.log(multiply);
+
+console.log(multiply.substr(0, 2));
+alert(`${multiply.substr(0, 2)}`);
+
+
+
 
 
 
