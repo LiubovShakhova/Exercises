9d32e5684e9b3bb103b84a5c108c219946314617
Liubov Shakhova <shakhova_l90@mail.ru>
2020-06-08 19:38:23 +0200

    chore lesson03 from lesson02

--- script.js
+++ script.js
@@ -25,3 +25,33 @@ console.log(addExpenses.split(', '));
 
 let budgetDay = money / 30;
 console.log(budgetDay);
+
+//Lesson03
+
+money = prompt('Ваш месячный доход?');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+deposit = confirm('Есть ли у вас депозит в банке?');
+
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let amount1 = +prompt('Во сколько это обойдется?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+let amount2 = +prompt('Во сколько это обойдется?');
+
+let budgetMonth = +money -(amount1 + amount2);
+console.log(`бюджет на месяц: ${budgetMonth}`);
+
+let budgetMonthCounter = Math.ceil(mission / budgetMonth);
+console.log(`за ${budgetMonthCounter} месяцев будет достигнута цель`);
+
+budgetDay = Math.floor(budgetMonth / 30);
+console.log(`дневной бюджет: ${budgetDay}`);
+
+if (budgetDay >= 1200) {
+  console.log('У вас высокий уровень дохода');
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+  console.log('У вас средний уровень дохода');
+} else if (budgetDay < 600 && budgetDay === 0) {
+  console.log('К сожалению, у вас уровень дохода ниже среднего');
+} else {
+  console.log('Что то пошло не так');
+}
