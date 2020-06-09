3c4d3bc8ba6eaa84cc81b20ac956311e0d0f34d3
LiubovShakhova <63476452+LiubovShakhova@users.noreply.github.com>
2020-06-09 20:20:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -48,9 +48,9 @@ console.log(`дневной бюджет: ${budgetDay}`);
 
 if (budgetDay >= 1200) {
   console.log('У вас высокий уровень дохода');
-} else if (budgetDay >= 600 && budgetDay < 1200) {
+} else if ( budgetDay > 600 && budgetDay < 1200) {
   console.log('У вас средний уровень дохода');
-} else if (budgetDay < 600 && budgetDay === 0) {
+} else if (budgetDay <= 600 && budgetDay >= 0) {
   console.log('К сожалению, у вас уровень дохода ниже среднего');
 } else {
   console.log('Что то пошло не так');
