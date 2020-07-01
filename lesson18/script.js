a6a4a6580bbc5bb09622357ff4ebca0e8dcd9d57
Liubov Shakhova <shakhova_l90@mail.ru>
2020-07-02 00:56:20 +0200

    lesson18

---
+++ lesson18/script.js
@@ -0,0 +1,46 @@
+
+
+const greeting = document.getElementById('greeting'),
+	today = document.getElementById('today'),
+	timeNow = document.getElementById('time_now');
+const date = new Date(),
+	newYear = new Date('December 31, 2020').getTime(),
+	todayDate = new Date().getTime();
+let waitNewYear = document.getElementById('wait_new_year');
+
+const getDate = () => {
+	let hours = date.getHours(),
+		minutes = date.getMinutes(),
+		seconds = date.getSeconds(),
+		hoursName;
+
+
+	if (hours >= 6 && hours <= 11) {
+		greeting.textContent = 'Доброе утро!';
+	} else if (hours > 11 && hours <= 17) {
+		greeting.textContent = 'Добрый день!';
+	} else if (hours > 17 && hours <= 23) {
+		greeting.textContent = 'Добрый вечер!';
+	} else {
+		greeting.textContent = 'Доброй ночи!';
+	}
+
+	const arr = new Array('Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота');
+	const weekDay = date.getDay();
+	today.textContent = `Сегодня: ${arr[weekDay]}`;
+
+	if (hours < 10) {
+		hours = `0${hours}`;
+	} else if (minutes < 10) {
+		minutes = `0${minutes}`;
+	} else if (seconds < 10) {
+		seconds = `0${seconds}`;
+	}
+	hoursName = hours <= 12 ? 'AM' : 'PM';
+	timeNow.textContent = `Текущее время: ${hours}:${minutes}:${seconds} ${hoursName}`;
+
+	const timeBefore = Math.floor((newYear - todayDate) / 1000 / 3600 / 24);
+	waitNewYear.textContent = `До нового года осталось ${timeBefore} дней`;
+};
+
+getDate();
---
+++ script/script.js
@@ -0,0 +1,45 @@
+
+window.addEventListener('DOMContentLoaded', () => {
+	//Timer
+	function countTimer(deadline) {
+		const timerHours = document.querySelector('#timer-hours'),
+			timerMinutes = document.querySelector('#timer-minutes'),
+			timerSeconds = document.querySelector('#timer-seconds');
+
+		function getTimeRemaining() {
+			const dateStop = new Date(deadline).getTime(),
+				dateNow = new Date().getTime(),
+				timeRemaining = (dateStop - dateNow) / 1000,
+				seconds = Math.floor(timeRemaining % 60),
+				minutes = Math.floor((timeRemaining / 60) % 60),
+				hours = Math.floor(timeRemaining / 60 / 60);
+			return { timeRemaining, hours, minutes, seconds };
+		}
+
+		function updateClock() {
+			const timer = getTimeRemaining();
+			timerHours.textContent = timer.hours;
+			timerMinutes.textContent = timer.minutes;
+			timerSeconds.textContent = timer.seconds;
+
+		if (timer.timeRemaining < 0) {
+      timerHours.textContent = '00';
+      timerMinutes.textContent = '00';
+      timerSeconds.textContent = '00';
+		} else { 
+      timerHours.textContent = timer.hours < 10 ? `0${timer.hours}` : timer.hours;
+      timerMinutes.textContent = timer.minutes < 10 ? `0${timer.minutes}` : timer.minutes;
+      timerSeconds.textContent = timer.seconds < 10 ? `0${timer.seconds}` : timer.seconds;
+    }
+		}
+		setInterval(updateClock, 1000);
+	}
+	countTimer('5 july 2020');
+
+
+
+
+
+
+
+});
