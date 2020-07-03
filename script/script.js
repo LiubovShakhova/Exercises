3ad21e4666f9c1ed5aff9281b9de3268ce2da496
Liubov Shakhova <shakhova_l90@mail.ru>
2020-07-03 23:23:27 +0200

    lesson21

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 	//Timer
-	function countTimer(deadline) {
+	 function countTimer(deadline) {
 		const timerHours = document.querySelector('#timer-hours'),
 			timerMinutes = document.querySelector('#timer-minutes'),
 			timerSeconds = document.querySelector('#timer-seconds');
@@ -36,15 +36,12 @@ window.addEventListener('DOMContentLoaded', () => {
 	}
 	countTimer('5 july 2020');
 
+	const	smoothScroll = elem => elem.scrollIntoView({ behavior: "smooth" });
 	//Menu
 	const toggleMenu = () => {
 		const btnMenu = document.querySelector('.menu'),
-			menu = document.querySelector('menu'),
-			closeBtn = document.querySelector('.close-btn'),
-			menuItems = menu.querySelectorAll('ul > li'),
-			scrollBtn = document.querySelector('a[href="#service-block"]');
+			menu = document.querySelector('menu');
 
-		const	smoothScroll = elem => elem.scrollIntoView({ behavior: "smooth" });
 		const handlerMenu = () => menu.classList.toggle('active-menu');
 
 		document.body.addEventListener('click', event => {
@@ -54,7 +51,7 @@ window.addEventListener('DOMContentLoaded', () => {
 			}
 
 			target = target.closest('menu > ul > li > a');
-			
+
 			if (target) {
 				event.preventDefault();
 				smoothScroll(document.querySelector(target.hash));
@@ -62,13 +59,20 @@ window.addEventListener('DOMContentLoaded', () => {
 			//если клик произошел мимо меню, оно закрывается
 			} else if (menu.classList.contains('active-menu')) {
 				handlerMenu();
-			} else if (scrollBtn) {
-				event.preventDefault();
-				smoothScroll(document.querySelector(scrollBtn.hash));
 			}
 		});
 	};
 	toggleMenu();
+
+	// переход на другой слайд
+	const btnDown = () => {
+		const scrollBtn = document.querySelector('a[href="#service-block"]');
+		scrollBtn.addEventListener("click", () => {
+			event.preventDefault();
+			smoothScroll(document.querySelector(scrollBtn.hash));
+		});
+	};
+	btnDown();
 
 	//popup
 	const togglePopUp = () => {
@@ -143,4 +147,106 @@ window.addEventListener('DOMContentLoaded', () => {
 		});
 	};
 	tabs();
+
+	//Slider
+	const slider = () => {
+		const slide = document.querySelectorAll('.portfolio-item'),
+			btn = document.querySelectorAll('portfolio-btn'),
+			slider = document.querySelector('.portfolio-content');
+
+		let currentSlide = 0,
+				interval,
+				dot = document.querySelectorAll('.dot');
+
+		const prevSlide = (elem, index, strClass) => {
+			elem[index].classList.remove(strClass);
+		};
+
+		const nextSlide = (elem, index, strClass) => {
+			elem[index].classList.add(strClass);
+		};
+
+		const autoPlaySlide = () => {
+
+			prevSlide(slide, currentSlide, 'portfolio-item-active');
+			prevSlide(dot, currentSlide, 'dot-active');
+			currentSlide++;
+			if (currentSlide >= slide.length) {
+				currentSlide = 0;
+			}
+			nextSlide(slide, currentSlide, 'portfolio-item-active');
+			nextSlide(dot, currentSlide, 'dot-active');
+		};
+
+		const startSlide = (time = 3000) => {
+			interval = setInterval(autoPlaySlide, time);
+		};
+
+		const stopSlide = () => {
+			clearInterval(interval);
+		};
+
+		slider.addEventListener('click', event => {
+			event.preventDefault();
+			let target = event.target;
+
+			if (!target.matches('.portfolio-btn, .dot')) {
+				return;
+			}
+			prevSlide(slide, currentSlide, 'portfolio-item-active');
+			prevSlide(dot, currentSlide, 'dot-active');
+
+			if (target.matches('#arrow-right')) {
+				currentSlide++;
+			} else if (target.matches('#arrow-left')) {
+				currentSlide--;
+			} else if (target.matches('.dot')) {
+				dot.forEach((elem, index) => {
+					if (elem === target) {
+						currentSlide = index;
+					}
+				});
+			}
+
+			if (currentSlide >= slide.length) {
+				currentSlide = 0;
+			}
+			if (currentSlide < 0) {
+				currentSlide = slide.length - 1;
+			}
+
+			nextSlide(slide, currentSlide, 'portfolio-item-active');
+			nextSlide(dot, currentSlide, 'dot-active');
+		});
+
+		slider.addEventListener('mouseover', (event) => {
+			if (event.target.matches('.portfolio-btn') ||
+			event.target.matches('.dot')) {
+				stopSlide();
+			}
+		});
+		slider.addEventListener('mouseout', (event) => {
+			if (event.target.matches('.portfolio-btn') ||
+			event.target.matches('.dot')) {
+				startSlide();
+			}
+		});
+
+		//точки с классом dot
+		const getDots = () => {
+			slide.forEach((item, index) => {
+				const li = document.createElement('li');
+				li.classList.add('dot');
+				if(index === 0) {
+						li.classList.add('dot-active');
+				}
+				document.querySelector('.portfolio-dots').append(li);
+				});
+				dot = document.querySelectorAll('.dot');
+		};
+
+		getDots();
+		startSlide(3000);
+	};
+	slider();
 });
