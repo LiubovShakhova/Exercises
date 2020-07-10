87b8dbf855df999b3eb8c3a949d92c4218a80a5f
Liubov Shakhova <shakhova_l90@mail.ru>
2020-07-10 12:36:03 +0200

    lesson24

--- script/script.js
+++ script/script.js
@@ -258,7 +258,7 @@ window.addEventListener('DOMContentLoaded', () => {
 			const target = event.target;
 
 			if (target.classList.contains("command__photo")) {
-				let src = target.src;
+				const src = target.src;
 				target.src = target.dataset.img;
 				target.dataset.img = src;
 			}
@@ -275,11 +275,52 @@ window.addEventListener('DOMContentLoaded', () => {
 		const validation = event => {
 			const target = event.target;
 
-			if (target.classList.contains('calc-item')) {
-					target.value = target.value.replace(/\D/, "");
-			} 
+			if (target.tagName === 'INPUT') {
+				target.value = target.value.replace(/\D/, "");
+			}
 		};
 		calcBlock.addEventListener("input", validation);
 	};
-	calculator();
+	calculator(); 
+
+	const calc = (price = 100) => {
+		const calcBlock = document.querySelector('.calc-block'),
+			calcType = document.querySelector('.calc-type'),
+			calcSquare = document.querySelector('.calc-square'),
+			calcDay = document.querySelector('.calc-day'),
+			calcCount = document.querySelector('.calc-count'),
+			totalValue = document.getElementById('total');
+
+		const countSum = () => {
+			let total = 0,
+					countValue = 1,
+					dayValue = 1;
+			const typeValue = calcType.options[calcType.selectedIndex].value,
+					squareValue = +calcSquare.value;
+
+			if (calcCount.value > 1) {
+				countValue += (calcCount.value - 1) / 10;
+			}
+
+			if (calcDay.value && calcDay.value < 5) {
+				dayValue *= 2;
+			} else if (calcDay.value && calcDay.value < 10) {
+				dayValue *= 1.5;
+			}
+
+			if (typeValue && squareValue) {
+				total = price * typeValue * squareValue * countValue * dayValue;
+			}
+			totalValue.textContent = total;
+		};
+
+		calcBlock.addEventListener('change', (event) => {
+			const target = event.target;
+
+			if (target.matches('select') || target.matches('input')) {
+				countSum();
+			}
+		});
+	};
+	calc(100);
 });
