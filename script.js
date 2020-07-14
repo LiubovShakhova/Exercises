5f0c162d9f59fa0b3a8ddebfe1464502384448cd
Liubov Shakhova <shakhova_l90@mail.ru>
2020-07-15 00:04:52 +0200

    lesson28-hard

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    const select = document.getElementById('cars'),
+        output = document.getElementById('output');
+
+    const sendRequest = () => {
+        return new Promise( (resolve, reject) => {
+            const request = new XMLHttpRequest();
+            request.open('GET', './cars.json');
+            request.addEventListener('readystatechange', () => {
+            if (request.readyState !== 4) {
+                return;
+            }
+            if (request.status === 200) {
+                resolve(); 
+            } else {
+                reject(request.statusText);
+            }
+            });
+            request.setRequestHeader('Content-Type', 'application/json');
+            request.send();
+        });
+    };
+        
+
+    select.addEventListener('change', () => {
+        sendRequest()
+            .then((request) => {
+                const data = JSON.parse(request.responseText);
+                data.cars.forEach(item => {
+                    if (item.brand === select.value) {
+                        const {brand, model, price} = item;
+                        output.innerHTML = `Тачка ${brand} ${model} <br>
+                        Цена: ${price}$`;
+                    }
+                });
+            })
+            .catch(error => {
+                output.innerHTML = 'Произошла ошибка';
+            });
+        
+    });
+
+
+});
